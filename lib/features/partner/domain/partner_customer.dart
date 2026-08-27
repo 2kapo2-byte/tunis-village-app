7cc17cb81d3da33154eb6571cc952ade1d3ff850
2kapo2-byte <2kapo2@gmail.com>
2026-08-27 05:38:56 +0300

    feat: add partner customer domain model

---
+++ lib/features/partner/domain/partner_customer.dart
@@ -0,0 +1,7 @@
+class PartnerCustomer {
+  const PartnerCustomer({required this.id, required this.fullName, this.phone, this.email});
+  final String id;
+  final String fullName;
+  final String? phone;
+  final String? email;
+}
