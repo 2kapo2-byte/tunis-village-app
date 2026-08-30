3cd03a5362d57cdf1cd3d20f688b50da1b06a01b
2kapo2-byte <2kapo2@gmail.com>
2026-08-30 07:05:37 +0300

    feat: expose shared network and analytics contracts

--- packages/tunis_village_core/lib/tunis_village_core.dart
+++ packages/tunis_village_core/lib/tunis_village_core.dart
@@ -2,3 +2,5 @@ library tunis_village_core;
 
 export 'src/models/guest_composition.dart';
 export 'src/models/booking.dart';
+export 'src/network/network_result.dart';
+export 'src/analytics/analytics_event.dart';
